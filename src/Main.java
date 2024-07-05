6baf9656f063e8208870648b56f19437cc4ac833
Dan Losev <losev@qtim.ru>
2024-07-06 02:49:15 +0300

    Added calculator with solution

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Calculator calc =  Calculator.instance.get();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int a = calc.plus.apply(1, 2); // 3
+        int b = calc.minus.apply(1, 1); // 0
+
+
+
+        /* Ошибка возникает при попытке деления на ноль
+         * А именно: 3 / 0
+         * int c = calc.devide.apply(a, b);
+         *
+         * Проблема решается путём обработки исключения
+         */
+
+        // Решение:
+        try {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Ошибка: Деление на ноль недопустимо!");
         }
     }
 }
