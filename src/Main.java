ae13f043659334746ea533d6684fe4d5fd6b3d5c
Dan Losev <losev@qtim.ru>
2024-07-14 21:44:54 +0300

    done with task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StringBuilder log = new StringBuilder();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String projectDir = System.getProperty("user.dir") + "/src";
+
+        // Директории src, res, savegames и temp
+        createDir(projectDir + "/Games/src", log);
+        createDir(projectDir + "/Games/res", log);
+        createDir(projectDir + "/Games/savegames", log);
+        createDir(projectDir + "/Games/temp", log);
+
+        // Директории main и test
+        createDir(projectDir + "/Games/src/main", log);
+        createDir(projectDir + "/Games/src/test", log);
+
+        // Директории drawables, vectors и icons
+        createDir(projectDir + "/Games/res/drawables", log);
+        createDir(projectDir + "/Games/res/vectors", log);
+        createDir(projectDir + "/Games/res/icons", log);
+
+        // Файлы Main.java, Utils.java и temp.txt
+        createFile(projectDir + "/Games/src/main/Main.java", log);
+        createFile(projectDir + "/Games/src/main/Utils.java", log);
+        createFile(projectDir + "/Games/temp/temp.txt", log);
+
+        writeLog(projectDir + "/Games/temp/temp.txt", log);
+    }
+
+    private static void createDir(String path, StringBuilder log) {
+        File dir = new File(path);
+
+        if (dir.mkdirs()) {
+            log.append("Directory created: ").append(path).append('\n');
+        } else {
+            log.append("Failed to create directory: ").append(path).append('\n');
+        }
+    }
+
+    private static void createFile(String path, StringBuilder log) {
+        File file = new File(path);
+
+        try {
+            if (file.createNewFile()) {
+                log.append("File created: ").append(path).append('\n');
+            } else {
+                log.append("Failed to create file: ").append(path).append('\n');
+            }
+        } catch (IOException e) {
+            log.append("Error creating file: ").append(path).append(" - ").append(e.getMessage()).append('\n');
+        }
+    }
+
+    private static void writeLog(String path, StringBuilder log) {
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write(log.toString());
+        } catch (IOException e) {
+            System.out.println("Error writing log to file: " + e.getMessage());
         }
     }
 }
